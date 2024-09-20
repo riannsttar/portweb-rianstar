a00695a6bf6535fb873bd9196e09836a1d00d858
riannsttar <softtokito@gmail.com>
2024-09-20 14:15:40 -0400

    AQL

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// Add JS here
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclik = () => {
+    menuIcont.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+}
